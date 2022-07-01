fd57bbf350a26ea57dd6d4d0d75bb374c2cb8889
Eric <equality509@gmail.com>
2022-07-01 23:09:11 +0700

    yelpcamp 2nd add

--- seeds/index.js
+++ seeds/index.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
-const Campground = require('../models/campground');
+const Campground = require('../models/campgrounds');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
-    useCreateIndex: true,
+  //  useCreateIndex: true,
     useUnifiedTopology: true
 });
 
